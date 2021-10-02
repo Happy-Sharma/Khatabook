a1147cbc50cdb8f9d5d5846a6110432f51d2739a
Vivek Kamboj <43985107+Vivek-Kamboj@users.noreply.github.com>
2021-10-02 12:45:34 +0530

    #2 Updated Khatabook book dashboard.

--- index.cpp
+++ index.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	int n;
 	cout << "Enter number of items in the list\n";
 	cin << n;
-	
-	string list[n];
+	vector<string> list(n,"");
 	cout << "Enter your list.\n";
 	for(int i=0; i<n; i++){
 		cin >> list[i];	
 	}
-	
+	cout<<"Thanks for entering list. This is a list:\n"
 	for(int i=0; i<n; i++){
-		cout << "Item " << i+1 << ": " list[i];	
+		cout << "Item " << i+1 << ": " list[i]<<"\n";	
 	}
 	cout << "List ends here!"
 	
